bc6858b2c892b4c04039b079ba589933a0b2afdd
makimenko <mihails.akimenko@gmail.com>
2017-08-31 23:23:10 +0300

    working

--- src/sample.js
+++ src/sample.js
@@ -8,13 +8,27 @@ console.log("Setup...");
 wpi.wiringPiSetup();
 wpi.mcp23017Setup(pin_base, i2c_addr);
 
+var myPin = pin_base + 8;
 
-console.log("Running...");
+console.log("on...");
+wpi.pinMode(myPin, 1);
+wpi.digitalWrite(myPin, 1);
+/*
 for (var i = pin_base; i < pin_base + count; i++) {
     wpi.pinMode(i, 1);
     wpi.digitalWrite(i, 1);
 }
-wpi.delay(1000);
+*/
+wpi.delay(2000);
+
+console.log("off...");
+wpi.digitalWrite(myPin, 0);
+/*
+for (var i = pin_base; i < pin_base + count; i++) {
+    wpi.digitalWrite(i, 0);
+}
+*/
+wpi.delay(2000);
 
 
 console.log("End.");
