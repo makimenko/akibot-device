9ae7118b5587adacea4b7f63a1ccdb91a83d55bd
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-09-05 13:18:22 +0300

    wip

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
 declare module "akibot-device" {
 
-    export class HMC5883L {
+    declare class HMC5883L {
         constructor(address: number);
         readMag(): { x, y, z };
     }
 
-    export class MCP23017 {
+    declare class MCP23017 {
         constructor(pinBase: number, addrI2C: number);
         pinMode(pin: number, mode: number);
         digitalWrite(pin: number, value: number);
     }
-    
+
 }
