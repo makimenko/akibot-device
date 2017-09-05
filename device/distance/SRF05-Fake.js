7bb30b55002cbf4e92aa6bc00f555fcc40635ea4
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-09-05 17:14:21 +0300

    fix

--- device/distance/SRF05-Fake.js
+++ device/distance/SRF05-Fake.js
@@ -2,8 +2,7 @@ class SRF05 {
 
     constructor(triggerPin, echoPin) {
         console.warn("WARNING: FAKE IMPLEMENTATION OF SRF05");
-        this.pinBase = pinBase;
-        this.addrI2C = addrI2C;
+        
     }
 
     getDistanceMm() {
